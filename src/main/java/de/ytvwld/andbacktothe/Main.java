9052761cd954576fe3519194fbc2fa76501b6c4e
Niklas Sombert <niklas@ytvwld.de>
2017-01-08 14:25:59 +0100

    Add a little GUI for configuring backup transports.

--- src/main/java/de/ytvwld/andbacktothe/Main.java
+++ src/main/java/de/ytvwld/andbacktothe/Main.java
@@ -7,13 +7,22 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 
 public class Main extends Activity
 {
     private static final String TAG = "AndBackToThe";
     private Context context = this;
     private BackupManager backupManager;
+    private Switch toggleBackup;
+    private Spinner currentTransportSpinner;
 
     /** Called when the activity is first created. */
     @Override
@@ -22,13 +31,77 @@ public class Main extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        // get the backupManager
         backupManager = new BackupManager(context);
+
+        // get our controls
+        toggleBackup = (Switch) findViewById(R.id.toggle_backup);
+        currentTransportSpinner = (Spinner) findViewById(R.id.spinner_current_transport);
+
         try
         {
+          // load the data
+
           boolean backupEnabled = (boolean)(backupManager.getClass().getMethod("isBackupEnabled").invoke(backupManager));
           String currentTransport = (String)(backupManager.getClass().getMethod("getCurrentTransport").invoke(backupManager));
+          String[] availableTransports = (String[])(backupManager.getClass().getMethod("listAllTransports").invoke(backupManager));
+          // move the current transport to the first position
+          availableTransports[Arrays.asList(availableTransports).indexOf(currentTransport)] = availableTransports[0];
+          availableTransports[0] = currentTransport;
+
           Log.v(TAG, "Backup enabled: " + backupEnabled);
-          Log.v(TAG, "Transport: " + currentTransport);
+          toggleBackup.setChecked(backupEnabled);
+          Log.v(TAG, "Current transport: " + currentTransport);
+          Log.v(TAG, "Available transports: ");
+          for(String transport: availableTransports)
+          {
+            Log.v(TAG, " " + transport);
+          }
+          ArrayAdapter<String> adapter = new ArrayAdapter(context, android.R.layout.simple_spinner_item, availableTransports);
+          adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+          //adapter.setNotifyOnChange(false); // Don't act on our changes.
+          currentTransportSpinner.setAdapter(adapter);
+
+          // set the listeners
+
+          toggleBackup.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton button, boolean isChecked) {
+              Log.v(TAG, "Setting backup to: " + isChecked);
+              try
+              {
+                backupManager.getClass().getMethod("setBackupEnabled", new Class[] {boolean.class}).invoke(backupManager, isChecked);
+              }
+              catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e)
+              {
+                Log.wtf(TAG, "We are a system app, but couldn't enable or disable the backup.");
+                Log.wtf(TAG, e);
+                System.exit(-1);
+              }
+            }
+          });
+
+          currentTransportSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
+            {
+              String selected = (String)parent.getItemAtPosition(pos);
+              Log.v(TAG, "User selected: " + selected);
+              try
+              {
+                backupManager.getClass().getMethod("selectBackupTransport", new Class[] {String.class}).invoke(backupManager, selected);
+              }
+              catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e)
+              {
+                Log.wtf(TAG, "We are a system app, but couldn't set the backup transport.");
+                Log.wtf(TAG, e);
+                System.exit(-1);
+              }
+            }
+
+            public void onNothingSelected(AdapterView <?> parent)
+            {
+              Log.v(TAG, "User selected nothing.");
+            }
+          });
         }
         catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e)
         {
